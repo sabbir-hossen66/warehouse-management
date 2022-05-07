ee4cd308ef0727c2bc1002b386b0bef40f674560
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-05-07 08:28:41 +0600

    insert database connect to mongodb

--- src/Pages/AddCategory/AddCategory.js
+++ src/Pages/AddCategory/AddCategory.js
@@ -4,16 +4,34 @@ import { useForm } from "react-hook-form";
 
 const AddCategory = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/category`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
 
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h2>Different Category Books Added</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("firstName", { required: true, maxLength: 20 })} />
-                <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-                <input type="number" {...register("age", { min: 18, max: 99 })} />
-                <input type="submit" />
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input className='mb-2' placeholder='name' {...register("name", { required: true, maxLength: 20 })} />
+                <input className='mb-2' placeholder='email' {...register("email")} />
+                <input className='mb-2' placeholder='quantity' {...register("quantity")} />
+                <input className='mb-2' placeholder=' supplier' {...register("supplier")} />
+                <input className='mb-2' placeholder='price' type="number" {...register("price")} />
+                <input className='mb-2' placeholder='photo url' type="text" {...register("img")} />
+                <textarea className='mb-2' placeholder=' description'{...register("description")} />
+                <input type="submit" value="Add Category" />
             </form>
 
         </div>
