8318011acff541c3261eff54256d6ebe71a8ae3a
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-05-08 23:11:24 +0600

    about section added

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -12,34 +12,36 @@ import { BsTelephoneFill } from 'react-icons/bs';
 
 const About = () => {
     return (
-        <div className='pb-5'>
-            <h2 className='about-header'>Contact Us</h2>
+        <div className='container  '>
+            <div className='mx-auto'>
+                <h2 className='about-header'>Contact Us</h2>
 
-            <div className='flex justify-content-center'>
-                <div className='left-side me-5'>
-                    <div className=' social-style'>
-                        <h3 className='font-style'> <FaAddressBook></FaAddressBook> Address</h3>
-                        112/B - Road 1200 building<br />
-                        /new kocukhet, Bangladesh
-                    </div><br />
+                <div className='row justify-content-center'>
+                    <div className='left-side me-5'>
+                        <div className=' social-style'>
+                            <h3 className='font-style'> <FaAddressBook></FaAddressBook> Address</h3>
+                            112/B - Road 1200 building<br />
+                            /new kocukhet, Bangladesh
+                        </div><br />
 
-                    <div className=' social-style'>
-                        <h3 className='font-style'><BsTelephoneFill></BsTelephoneFill>  phone No</h3>
-                        +007894145478 <br />
-                        4587789413635
+                        <div className=' social-style'>
+                            <h3 className='font-style'><BsTelephoneFill></BsTelephoneFill>  phone No</h3>
+                            +007894145478 <br />
+                            4587789413635
+                        </div>
+
                     </div>
+                    <div className="right-side">
+                        <div className=' social-style'>
+                            <h3 className='font-style'><GrMail></GrMail>  Mail</h3>
+                            yourmail.dhaka@domain.com <br />
+                            houserent.rajbari@domain.com
+                        </div><br />
 
-                </div>
-                <div className="right-side">
-                    <div className=' social-style'>
-                        <h3 className='font-style'><GrMail></GrMail>  Mail</h3>
-                        yourmail.dhaka@domain.com <br />
-                        houserent.rajbari@domain.com
-                    </div><br />
-
-                    <div className='social-style'>
-                        <h3 className='font-style'><FaUserCircle></FaUserCircle> Social Account </h3>
-                        <h2><span className='social-icon-style'><BsFacebook></BsFacebook></span> <span className='social-icon-style'><AiFillTwitterCircle></AiFillTwitterCircle></span>  <span className='social-icon-style'><ImLinkedin></ImLinkedin></span></h2>
+                        <div className='social-style'>
+                            <h3 className='font-style'><FaUserCircle></FaUserCircle> Social Account </h3>
+                            <h2><span className='social-icon-style'><BsFacebook></BsFacebook></span> <span className='social-icon-style'><AiFillTwitterCircle></AiFillTwitterCircle></span>  <span className='social-icon-style'><ImLinkedin></ImLinkedin></span></h2>
+                        </div>
                     </div>
                 </div>
             </div>
